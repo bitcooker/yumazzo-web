2ea8a9cb9e8c046261db740717ee729050b00213
bitcooker <dreamhighpassionhigh@gmail.com>
2023-07-25 08:30:56 +0800

    feat: add text component

--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -1,2 +1,3 @@
 export * from './inputs'
 export * from './button'
+export * from './text'
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
 export * from './common'
 export * from './search-box'
 export * from './flag-box'
+export * from './difficulty-box'
