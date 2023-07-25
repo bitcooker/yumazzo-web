da488b4a1ef25c88cbf07071834b4b3d3ec83d0d
bitcooker <dreamhighpassionhigh@gmail.com>
2023-07-25 10:36:14 +0800

    fix: add text, outline color type to button component

--- src/components/common/button/Button.tsx
+++ src/components/common/button/Button.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import clsx from 'clsx';
 
 interface IButtonProps {
-  color?: 'primary' | 'secondary';
+  color?: 'primary' | 'secondary' | 'text' | 'outline';
   variant?: 'rounded' | 'pill';
   disabled?: boolean;
   children: React.ReactNode;
@@ -15,6 +15,9 @@ const colorClassNames: Record<string, string> = {
     'bg-[#7E51FF] hover:bg-[#663CDD] active:bg-white active:text-black focus:border focus:border-[#663CDD] focus:ring-4 focus:ring-[#B89FFF] disabled:bg-[#9B78FF] disabled:text-[#B4C5FF]',
   secondary:
     'bg-[#171F2F] hover:bg-[#131823] active:bg-[#A6ACC4] active:text-black focus:border focus:border-[#663CDD] focus:ring-4 focus:ring-[#B89FFF] disabled:bg-[#171F2F] disabled:text-[#8C92AB]',
+  text: 'hover:bg-[#B89FFF] hover:text-[#280363] active:bg-[#9B78FF] active:text-white focus:ring-4 focus:text-white focus:ring-[#D5C6FF] disabled:text-[#171F2F]',
+  outline:
+    'border border-[#5B6178] bg-[#131823] text-[#E9EAF6] hover:border-[#D5C6FF] hover:text-[#D5C6FF] active:border-[#9B78FF] active:text-[#9B78FF] focus:ring-4 focus:text-white focus:ring-[#B89FFF] disabled:text-[#171F2F]',
 };
 
 const variantClassNames: Record<string, string> = {
