c503c48fa6c4d57e176994dd359162baa4c2fd23
bitcooker <dreamhighpassionhigh@gmail.com>
2023-07-25 22:45:59 +0800

    feat: add backend api for submitting new recipe

---
+++ src/app/api/recipes/route.ts
@@ -0,0 +1,23 @@
+import { NextResponse } from "next/server";
+
+export async function POST(request: Request) {
+    const body = await request.json();
+
+    const url = `${process.env.NEXT_PUBLIC_API_ENDPOINT}/yumazoo/recipes`;
+    const response = await fetch(url, {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(body)
+    });
+    const data = await response.json();
+    if (response.status == 201) {
+        return NextResponse.json({ data: data });
+    }
+    else if (response.status == 422) {
+        return NextResponse.json({ error: data.detail[0].msg });
+    }
+    else return NextResponse.json({ error: "Unexpected Error Occured!" });
+}
